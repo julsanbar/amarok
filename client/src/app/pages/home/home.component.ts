c5a9e1b5b6acdbe80ea15fc88df213550ac7508b
Julian <sanjuan611@gmail.com>
2021-05-08 22:34:53 +0200

    Actualizar home.component.css, home.component.html y home.component.ts

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ProductoService } from "../../services/producto.service";
+import { Producto } from "../../models/producto.model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges {
+
+  public srcMasVendidos: string[] = [];
+  public producto: Producto | undefined;
 
   constructor(private productoService: ProductoService) { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
 
   ngOnInit(): void {
 
+    this.imagenesProductosMasVendidos();
+
+  }
+
+  imagenesProductosMasVendidos(): void{
+
     this.productoService.productosMasVendidos().subscribe((res:any) => {
 
-      console.log(res.productos);
+      for (const producto of res.productos) {
+
+        this.srcMasVendidos.push("../../../assets/img/productos/"+producto.referencia+".jpg");
+
+      }
+
     });
 
   }
