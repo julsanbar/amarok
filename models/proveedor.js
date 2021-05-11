d4ad16e211771c00a06ffef2be057a564fec832e
Julian <sanjuan611@gmail.com>
2021-05-11 21:54:37 +0200

    Actualizar proveedor.js

--- models/proveedor.js
+++ models/proveedor.js
@@ -1,27 +1,117 @@
 const mongoose = require('mongoose');
 
+//Variables para las validaciones propias
+let validaReferencia = (referencia) => {
+
+const regExp = new RegExp(/^PR\d+$/);
+
+return (!referencia)? false: regExp.test(referencia.toString());
+
+};
+
+let validaNombre = (nombre) => {
+
+    const regExp = new RegExp(/^[a-zA-Z ]+$/);
+
+    return (!nombre)? false: regExp.test(nombre.toString());
+
+};
+
+let validaEmail = (email) => {
+
+    const regExp = new RegExp(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/);
+
+    return (!email)? false: regExp.test(email.toString());
+
+};
+
+let validaCif = (cif) => {
+
+    const regExp = new RegExp(/^[a-zA-Z0-9 ]+$/);
+
+    return (!cif)? false: !regExp.test(cif.toString());
+
+};
+
+let validaTelefono = (telefono) => {
+
+    const regExp = new RegExp(/^\d+$/);
+
+    return (!telefono)? false: regExp.test(telefono.toString());
+
+};
+
+///Variables usadas para establecer el mensaje personalizado que se guarda en el log
+const referenciaValidators = [
+{
+
+    validator: validaReferencia,
+    message: 'La referencia debe ser PR seguido de un número.'
+
+}
+];
+
+const nombreValidators = [
+{
+
+    validator: validaNombre,
+    message: 'El nombre debe estar compuesto por letras y/o espacios.'
+
+}
+];
+
+const emailValidators = [
+{
+
+    validator: validaEmail,
+    message: 'El email no es válido.'
+
+}
+];
+
+const cifValidators = [
+{
+
+    validator: validaCif,
+    message: 'El cif debe de estar compuesto por números, letras y/o espacios.'
+
+}
+];
+
+const telefonoValidators = [
+{
+
+    validator: validaTelefono,
+    message: 'El telefono debe estar compuesto por números.'
+
+}
+];
+
 const ProveedorScheme = new mongoose.Schema(
     {
 
         referencia: {
 
             type: String,
-            unique: true,
-            required: true
+            unique: [true, 'La referencia es necesaria.'],
+            required: [true, 'Ya existen esta referencia.'],
+            validate: referenciaValidators
 
         },
 
         nombre: {
 
             type: String,
-            required: true
+            required: [true, 'El campo nombre es necesario.'],
+            validate: nombreValidators
 
         },
 
         cif: {
 
             type: String,
-            required: true
+            required: [true, 'El campo cif es necesario.'],
+            validate: cifValidators
 
         },
 
@@ -40,15 +130,17 @@ const ProveedorScheme = new mongoose.Schema(
         email: {
 
             type: String,
-            required: true,
-            unique: true
+            required: [true, 'El campo email es necesario.'],
+            unique: [true, 'Ya existe este email.'],
+            validate: emailValidators
 
         },
 
         telefono: {
 
             type: String,
-            required: true
+            required: [true, 'El campo teléfono es necesario.'],
+            validate: telefonoValidators
 
         },
 
@@ -61,7 +153,7 @@ const ProveedorScheme = new mongoose.Schema(
         habilitado: {
 
             type: Boolean,
-            required: true
+            required: [true, 'El campo habilitado es necesario.']
 
         }
 
